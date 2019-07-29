97593c86308e9cc5b5e7bd5fad048e68ec10bdc1
Cellie <CelliesProjects@users.noreply.github.com>
2019-07-29 12:56:17 +0200

    Updated the simple example.

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,21 +1,22 @@
 #include <moonPhase.h>
 
-moonPhase moonPhase; /* include a MoonPhase instance */
+moonPhase moonPhase; // include a MoonPhase instance
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println( "moonPhase simple example." );
 
-  moonData_t moon; /* variable to receive the data */
+  moonData_t moon; // variable to receive the data
 
-  moon = moonPhase.getInfo(); //get the current moon phase (1/1/1970 at about 00:00:00 UTC )
+  moon = moonPhase.getPhase(); // gets the current moon phase (1/1/1970 at 00:00:00 UTC )
+                               // Because moonPhase uses the system time, which is not set in this example
 
   Serial.print( "Moon phase angle: " );
-  Serial.print( moon.angle );                       /* angle is a integer between 0-360  */
+  Serial.print( moon.angle );                       // angle is a integer between 0-360
   Serial.println( " degrees." );
   Serial.print( "Moon surface lit: " );
-  Serial.printf( "%.0f%%", moon.percentLit * 100 ); /* percentLit is a real between 0-1  */
+  Serial.print( moon.percentLit * 100 ); // percentLit is a real between 0-1
 }
 
 void loop() {
