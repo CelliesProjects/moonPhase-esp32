6b90027ad591b94ae400ca4b19718aa4ce1dd6d1
Cellie <CelliesProjects@users.noreply.github.com>
2025-12-22 20:50:21 +0100

    Refactor MoonPhase API, fix naming, and improve phase angle rounding

--- moonPhase.h
+++ moonPhase.h
@@ -13,26 +13,27 @@
 
 struct moonData_t
 {
-  int32_t angle;
-  double  percentLit;
+    int32_t angleDeg;
+    double amountLit;
 };
 
-class moonPhase
+class MoonPhase
 {
 public:
-  moonData_t getPhase(const time_t t)
-  {
-    struct tm timeinfo;
-    gmtime_r(&t, &timeinfo);
-    return _getPhase(1900 + timeinfo.tm_year, 1 + timeinfo.tm_mon, timeinfo.tm_mday, _fhour(timeinfo));
-  }
+    moonData_t getPhase(const time_t t)
+    {
+        struct tm timeinfo;
+        gmtime_r(&t, &timeinfo);
+        return _getPhase(1900 + timeinfo.tm_year, 1 + timeinfo.tm_mon, timeinfo.tm_mday, _fhour(timeinfo));
+    }
 
-  moonData_t getPhase()
-  {
-    return getPhase(time(NULL));
-  }
+    moonData_t getPhase()
+    {
+        return getPhase(time(NULL));
+    }
+
 private:
-  double     _fhour(const struct tm &timeinfo);
-  moonData_t _getPhase(const int32_t year, const int32_t month, const int32_t day, const double &hour);
+    double _fhour(const struct tm &timeinfo);
+    moonData_t _getPhase(const int32_t year, const int32_t month, const int32_t day, const double &hour);
 };
 #endif
