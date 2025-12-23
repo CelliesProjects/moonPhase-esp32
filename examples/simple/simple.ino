14b1ce963eff1a2f636136214f9763c423fd8654
Cellie <CelliesProjects@users.noreply.github.com>
2025-12-23 15:15:09 +0100

    Update example `simple.ino`

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,24 +1,25 @@
+#include <Arduino.h>
 #include <moonPhase.h>
 
-moonPhase moonPhase; // include a MoonPhase instance
+MoonPhase moonPhase; // include a MoonPhase instance
+moonData_t moon;     // variable to receive the data
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println( "moonPhase simple example." );
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("moonPhase simple example.");
 
-  moonData_t moon;                        // variable to receive the data
+    moon = moonPhase.getPhase(); // gets the current moon phase ( 1/1/1970 at 00:00:00 UTC )
 
-  moon = moonPhase.getPhase();            // gets the current moon phase ( 1/1/1970 at 00:00:00 UTC )
-
-  Serial.print( "Moon phase angle: " );
-  Serial.print( moon.angle );             // angle is a integer between 0-360
-  Serial.println( " degrees." );
-  Serial.print( "Moon surface lit: " );
-  Serial.print( moon.percentLit * 100 );  // percentLit is a real between 0-1
+    Serial.print("Moon phase angle: ");
+    Serial.print(moon.angleDeg); // angle is a integer between 0-360
+    Serial.println(" degrees.");
+    Serial.print("Moon surface fraction lit: ");
+    Serial.print(moon.amountLit); // amountLit is a real between 0-1
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
